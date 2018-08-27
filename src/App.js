cacc7f501f4555c5d8572018d778b94a69ef2c98
gilishWix <40733156+gilishWix@users.noreply.github.com>
2018-08-27 16:33:04 +0300

    add reducer and store with redux library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import HelloWorld from "./HelloWorld";
+import {createStore} from "redux"; //an import from the redux library
+import reducer from "./reducers";
+
+/* createStore takes in a few arguments. The first is a reducer. The store is always in sync with a reducer.
+    The reducer is the only mandatory argument passed into createStore() */
+const initialState = {tech: 'Redux'};
+const store = createStore(reducer, initialState);
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
-  render() {
-    return <HelloWorld tech={this.state.tech} />;
-  }
+    render() {
+        return <HelloWorld tech={store.getState().tech}/>;
+    }
 }
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+// For a Redux reducer, you always return the new state of your application.
+
+export default (state) => {
+    return state;
+}
