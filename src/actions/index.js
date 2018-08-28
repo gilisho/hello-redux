d3d607b7e5c13fa212a22789f272601dc2df0a2f
gilishWix <40733156+gilishWix@users.noreply.github.com>
2018-08-28 11:56:09 +0300

    update reducer with switch scope

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,3 @@
-const SET_TECHNOLOGY = "SET_TECHNOLOGY";
+import {SET_TECHNOLOGY} from "./action-consts";
 
-export const setTechnology = text => ({ type: SET_TECHNOLOGY, text });
+export const setTechnology = text => ({ type: SET_TECHNOLOGY, tech: text }); // actions creator
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,15 @@
+import {SET_TECHNOLOGY} from "../actions/action-consts";
+
 export default (state, action) => {
     console.log(action); // log whatever action comes into the reducer
-    return state; // the reducer returns the new state of the app
+    switch (action.type) {
+        case SET_TECHNOLOGY:
+            return {...state, tech: action.tech};
+
+        default:
+            return state; // the reducer returns the new state of the app
+    }
 }
 
-
-
+// Never mutate the state given to the reducer. instead - create a new state with the properties of the old state (use ...state).
+// every Reducer you write should be a pure function with no side-effects — No API calls or updating a value outside the scope of the function
